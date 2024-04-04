9d006d4e12a0da8200c5919c04710461dcbc5b19
Bryan O <bocampo91@yahoo.com>
2024-04-04 02:46:50 -0400

    made small change to html file, added more code to script file and started adding styling to CSS file

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,26 @@
-const citySubmit = document.getElementById('search');
-let citySearch = document.getElementById('citySearch');
+let citySubmit = document.getElementById('citySubmit');
+let citySearch = document.querySelector('#citySearch');
+const todayWeather = document.querySelector('#weatherDisplay');
 const weatherAPIKey = '9d3dbee189ccaae4b5d0e40a67f32667';
 
-const cityURL = `https://api.openweathermap.org/data/2.5/weather?q=Charlotte&appid=${weatherAPIKey}`
-
-
-
 function checkWeather() {
+
+    const cityURL = `https://api.openweathermap.org/data/2.5/weather?q=${citySearch.value}&appid=${weatherAPIKey}`
+
     fetch(cityURL).then(function (response) {
         return response.json();
     }).then(function (data) {
         console.log(data);
+
+        let weatherResultName = document.createElement('p');
+        let weatherIcon = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+        let weatherResultDate = document.createElement('p');
+
+        weatherResultName.textContent = `${data.name} ${weatherIcon}`;
+        //weatherResultDate.textContent = ;
+
+        todayWeather.appendChild(weatherResultName);
+
         checkForecast(data.coord.lon, data.coord.lat);
     })
 }
@@ -26,11 +36,9 @@ function checkForecast(lon, lat) {
     })
 }
 
-checkWeather();
-
 citySubmit.addEventListener('click', function (event) {
 
-
-
+    event.preventDefault();
+    checkWeather();
 })
 
